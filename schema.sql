52584e18827bff43e6bde54cf8ac40adeba95050
baheersbi <baseer.baheer@berlinsbi.com>
2024-04-19 11:24:45 +0200

    Table added

--- schema.sql
+++ schema.sql
@@ -1,5 +1,8 @@
-CREATE table Persons (
-    id int(3) auto_increment primary key,
-    name varchar(12) not null,
-    email varchar(45) not null
-)
+create database datapulse;
+
+CREATE TABLE Stores (
+                        StoreID INT AUTO_INCREMENT PRIMARY KEY,
+                        StoreName VARCHAR(255) NOT NULL,
+                        Location VARCHAR(255) NOT NULL,
+                        Contact VARCHAR(255)
+);
